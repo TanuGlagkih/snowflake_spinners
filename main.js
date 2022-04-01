52406c1c46ef9617afbfa173fb664241c9b5fac3
TanuGlagkih <96661925+TanuGlagkih@users.noreply.github.com>
2022-04-01 21:44:29 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+class Snow {
+    constructor () {
+    this.startTime = 0,
+    this.rAF,
+    this.rotateCount
+  }
+}
+
+var spinner = [];
+
+for(let j=0; j<5; j++) {
+  spinner[j] = document.createElement('div');
+  spinner[j].setAttribute('id', [j]);
+  spinner[j].innerHTML = '❆';
+  document.body.appendChild(spinner[j]);
+  spinner[j] = new Snow(); 
+}
+
+document.body.addEventListener('click', (event)=> {
+       
+    var i = event.target.getAttribute('id');
+    parseInt(i);
+
+      if (event.target.classList.contains('move')) { 
+       cancelAnimationFrame(spinner[i].rAF);
+       spinner[i].rotateCount = 0;
+       event.target.classList.remove('move');
+       } else {
+
+        function draw(timestamp) {             
+
+          if (!spinner[i].startTime) {
+              spinner[i].startTime = timestamp;
+          }
+           
+           spinner[i].rotateCount = (timestamp - spinner[i].startTime) / 6;
+           if (spinner[i].rotateCount > 359) {
+            spinner[i].rotateCount %= 360;
+           }
+         
+          event.target.style.transform = `rotate(${ spinner[i].rotateCount}deg)`;
+          spinner[i].rAF = requestAnimationFrame(draw);
+          event.target.classList.add('move');
+
+           }
+     draw();
+    }
+
+});
